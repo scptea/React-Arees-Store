bce1bf14be6ba4d76175fc1e33ef001521d99cc6
scptea <iscp506506@gmail.com>
2023-10-25 10:53:53 +0300

    JS 5 EX 2 ( read comment )

---
+++ react-arees-store/src/CoffeeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CoffeeComponent = () => {
+    return (
+        <div>
+            <p>Arees store for selling coffee equipment</p>
+            <p>Explore our range of high quality coffee machines and accessories</p>
+        </div>
+    );
+};
+
+export default CoffeeComponent;
---
+++ react-arees-store/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CoffeeComponent from './CoffeeComponent';
+
+ReactDOM.render(
+    <CoffeeComponent />,
+    document.getElementById('coffee-component-root')
+);
